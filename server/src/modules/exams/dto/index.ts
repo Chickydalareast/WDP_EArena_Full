48c24e7ff86fef5c679011b506e96052303f0562
Chicky <phantrunghieu3101@gmail.com>
2026-03-29 20:51:52 +0700

    delete

--- server/src/modules/exams/dto/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './auto-save.dto';
-export * from './init-manual.dto';
-export * from './update-paper-questions.dto';
-export * from './create-exam-assignment.dto';
-export * from './generate-matrix.dto';
--- server/src/modules/questions/dto/clone-question.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class CloneQuestionDto {
-  @IsMongoId()
-  @IsNotEmpty()
-  destFolderId: string; 
-}
